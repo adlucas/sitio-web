10cb76b9a0bfce343d738de18af85c068106642f
angel <delucas91@gmail.com>
2015-03-05 18:15:25 +0100

    sprite social, paginas añadidas

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,22 @@
-/* jshint devel:true */
-console.log('\'Allo \'Allo!');
+'use strict';
+      function initMap() {
+    var varLocation = new google.maps.LatLng(42.142581, -0.417403);
+ 
+        var varMapoptions = {
+          center: varLocation,
+          zoom: 16
+        };
+ var varMap;
+    var varMarker = new google.maps.Marker({
+      position: varLocation,
+            map: varMap,
+      title:'Huesca'});
+ 
+         varMap = new google.maps.Map(document.getElementById('map-container'),
+            varMapoptions);
+ 
+    varMarker.setMap(varMap); 
+ 
+      }
+ 
+      google.maps.event.addDomListener(window, 'load', initMap);
